9cf7555b596a164544038b909e24007d79326f68
Dominic <dominic.pr93@gmail.com>
2020-12-13 18:34:35 +0100

    general style and js fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 // general script operations
 
+// wait until dom elements are loaded
 window.addEventListener("DOMContentLoaded", (event) => {
 	// nav toggle
 	const burgerIcon = document.querySelector(".burger-icon");
 	const navMenu = document.querySelector(".nav-top__menu");
 	const bodyElement = document.querySelector("body");
 	const toggleNav = () => {
-		// TODO: dont toggle on desktop
+		if (window.matchMedia("(min-width: 1024px)").matches) return; // match with burger-menu breakpoint
 		burgerIcon.classList.toggle("opened");
 		navMenu.classList.toggle("opened");
 		bodyElement.classList.toggle("prevent-scrolling");
