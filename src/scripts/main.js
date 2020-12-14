d2553fef43f94fb5b3f9fd42b3adc6f6c6c65d27
Dominic <dominic.pr93@gmail.com>
2020-12-15 00:16:54 +0100

    added listener to from submit event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -109,4 +109,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
 			customModal.classList.remove("opened");
 		});
 	});
+
+	// validate form
+	document.querySelector("form").addEventListener("submit", (e) => {
+		e.preventDefault();
+		console.log("form submitted");
+	});
 });
